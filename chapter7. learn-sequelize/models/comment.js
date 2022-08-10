6310b118d34a73ec42a7826ef9879bb693d91829
Kimwater <iamkimwater@gmail.com>
2022-08-10 17:48:41 +0900

    [ADD] DB tables

--- chapter7. learn-sequelize/models/comment.js
+++ chapter7. learn-sequelize/models/comment.js
@@ -5,14 +5,20 @@ const { sequelize } = require(".");
 // 컬럼: 작성자, 댓글내용, 생성일
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define('comment', {
-		commenter: {
-
-		},
+		// commenter는 user와 관계있는 데이터이므로 index.js에 관계를 작성
+		// commenter: {
+		// 	type: DataTypes.STRING(20),
+		// 	allowNull: false,
+		// 	unique: true,
+		// },
 		comment: {
-
+			type: DataTypes.STRING(100),
+			allowNull: false,
 		},
 		created_at: {
-
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: sequelize.literal('now()'),
 		}
 	}, {
 		timestamps: false,
--- chapter7. learn-sequelize/models/user.js
+++ chapter7. learn-sequelize/models/user.js
@@ -8,19 +8,26 @@ module.exports = (sequelize, DataTypes) => {
 	// sequelize.define 안에 테이블 이름, 테이블 내용 들어감
 	return sequelize.define('user', {
 		name: {
-			type: DataTypes.STRING
+			type: DataTypes.STRING(20),   // type: 자료형, 글자수 20자 제한
+			allowNull: false,   // Null 허용할거니?
+			unique: true,   // 고유값이니?
 		},
 		age: {
-
+			type: DataTypes.INTEGER.UNSIGNED,   // unsigned: 음수X
+			allowNull: false,
 		},
 		married: {
-
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
 		},
 		comment: {
-
+			type: DataTypes.TEXT,
+			allowNull: true,
 		},
 		created_at: {
-
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: sequelize.literal('now()'),   // db한테 직접 알아내라고 지시
 		}
 	}, {
 		timestamps: false,   // 시퀄라이즈에게 생성일 자동으로 생성하도록 (생성일 따로 있으니 false처리)
